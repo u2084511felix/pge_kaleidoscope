6f2238e7cf308bc195de20b85cde9c3db1fdb789
u2084511felix <79988595+u2084511felix@users.noreply.github.com>
2025-09-03 20:23:28 +0100

    removed copyright from license

--- pgekaleidoscope.cpp
+++ pgekaleidoscope.cpp
@@ -1,9 +1,7 @@
 /*
+
 	License (OLC-3)
 	~~~~~~~~~~~~~~~
-
-	Copyright 2018 - 2025 OneLoneCoder.com
-
 	Redistribution and use in source and binary forms, with or without modification,
 	are permitted provided that the following conditions are met:
 
@@ -28,6 +26,8 @@
 	CONTRACT, STRICT LIABILITY, OR TORT	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 	SUCH DAMAGE.
+
+
 */
 
 #define OLC_PGE_APPLICATION
